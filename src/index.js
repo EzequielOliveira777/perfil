7b0367aa798ee81d562d6aaf3a2f06103349b244
hrkoliveira <herikoliveira@ymail.com>
2025-11-10 16:00:02 -0300

    calculadora inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculator from './main/Calculator';
+import * as serviceWorker from './serviceWorker';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <h1>TipsCode</h1>
+    <Calculator />
+  </React.StrictMode>
+);
+serviceWorker.unregister();
